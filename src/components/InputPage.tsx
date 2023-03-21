93f8ebd8c0315c08510b5df4d727774b30a11ac8
Afzal Nomani <afzalnomani1@gmail.com>
2023-03-21 11:33:00 +0530

    side bar click form render

--- src/components/InputPage.tsx
+++ src/components/InputPage.tsx
@@ -1,9 +1,4 @@
-import React, {
-  FormEvent,
-  FormEventHandler,
-  MouseEvent,
-  MouseEventHandler,
-} from "react";
+import React, { FormEvent, useState } from "react";
 import { InterfaceInputs } from "../Modal/Interfaces";
 import SidebarNav from "./SidebarNav";
 
@@ -16,15 +11,35 @@ interface IProp {
 }
 
 function InputPage(props: IProp) {
+  type SideBarTuples = [boolean, boolean, boolean, boolean];
+
+  const sideBarTuples: SideBarTuples = [true, false, false, false];
+
+  const [showForm, setShowForm] = useState<SideBarTuples>(sideBarTuples);
+
+  const showFormHandler = (event: React.MouseEvent<HTMLButtonElement>) => {
+    console.log(event.currentTarget.value);
+
+    setShowForm((prev) => {
+      const list = prev;
+
+      list[+event.currentTarget.value] = true;
+
+      return list;
+    });
+  };
+
+  console.log(showForm);
+
   return (
     <div className="w-1/2 flex gap-4 bg-slate-100 p-4">
       {/* <h2 className="text-2xl text-center font-bold">Report</h2> */}
 
-      <SidebarNav />
+      <SidebarNav onNavClick={showFormHandler} />
 
       <form
         action=""
-        className="bg-white p-10 basis-2/3"
+        className="bg-white flex flex-col gap-6 p-10 basis-2/3"
         onSubmit={props.generateReport}
       >
         <div className="w-full mb-4">
@@ -52,8 +67,23 @@ function InputPage(props: IProp) {
             placeholder="Last name"
           />
         </div>
+
         <div className="w-full mb-4">
-          <label htmlFor="gender">Gender</label>
+          <label htmlFor="age">Patient Age</label>
+          <input
+            className="border-2 border-gray-300 py-2 px-4 w-full rounded-md"
+            type="number"
+            name="age"
+            id="age"
+            value={props.inputs.age}
+            onChange={props.handleInputChange}
+          />
+        </div>
+
+        <div className=" flex gap-16 items-center w-full mb-4">
+          <label htmlFor="gender" className="m-0">
+            Gender
+          </label>
 
           <div className="flex gap-8">
             <div className="flex items-center gap-4 p-2">
@@ -76,18 +106,6 @@ function InputPage(props: IProp) {
               <input type="radio" name="gender" id="female" className="mt-0" />
             </div>
           </div>
-        </div>
-
-        <div className="w-full mb-4">
-          <label htmlFor="age">Patient Age</label>
-          <input
-            className="border-2 border-gray-300 py-2 px-4 w-full rounded-md"
-            type="number"
-            name="age"
-            id="age"
-            value={props.inputs.age}
-            onChange={props.handleInputChange}
-          />
         </div>
 
         <button
@@ -118,3 +136,6 @@ function InputPage(props: IProp) {
 }
 
 export default InputPage;
+function item(value: boolean, index: number, array: boolean[]): boolean {
+  throw new Error("Function not implemented.");
+}
--- src/components/SidebarNav.tsx
+++ src/components/SidebarNav.tsx
@@ -1,22 +1,49 @@
-import React from "react";
+import React, { useState } from "react";
 import { IoIosPersonAdd } from "react-icons/io";
 import { FaBookMedical, FaPrescription, FaMoneyBillAlt } from "react-icons/fa";
 
-function SidebarNav() {
+interface SidebarProp {
+  onNavClick: (e: React.MouseEvent<HTMLButtonElement>) => void;
+}
+
+function SidebarNav(props: SidebarProp) {
+  enum sideButtons {
+    personal,
+    history,
+    medicines,
+    bill,
+  }
+
   return (
     <div className="flex flex-col bg-slate-100 pl-2 p-6 gap-10 items-baseline basis-1/3 drop-shadow-md font-thin box-border">
-      <button className="flex items-center gap-4 hover:font-semibold hover:scale-105 border-b-2 border-cyan-500 transition-all duration-100 pl-1">
+      <button
+        value={sideButtons.personal}
+        onClick={props.onNavClick}
+        className="flex items-center gap-4 hover:font-semibold hover:scale-105 border-b-2 border-cyan-500 transition-all duration-100 pl-1"
+      >
         <IoIosPersonAdd /> Personal Information
       </button>
-      <button className="flex gap-4 items-center hover:font-bold hover:scale-105 border-b-2 border-cyan-500 transition-all duration-100 pl-1">
+      <button
+        value={sideButtons.history}
+        onClick={props.onNavClick}
+        className="flex gap-4 items-center hover:font-bold hover:scale-105 border-b-2 border-cyan-500 transition-all duration-100 pl-1"
+      >
         <FaBookMedical />
         Medical history
       </button>
-      <button className="flex gap-4 items-center hover:font-bold hover:scale-105 border-b-2 border-cyan-500 transition-all duration-100 pl-1">
+      <button
+        value={sideButtons.medicines}
+        onClick={props.onNavClick}
+        className="flex gap-4 items-center hover:font-bold hover:scale-105 border-b-2 border-cyan-500 transition-all duration-100 pl-1"
+      >
         <FaPrescription />
         Medicines
       </button>
-      <button className="flex gap-4 items-center hover:font-bold hover:scale-105 border-b-2 border-cyan-500 transition-all duration-100 pl-1">
+      <button
+        value={sideButtons.bill}
+        onClick={props.onNavClick}
+        className="flex gap-4 items-center hover:font-bold hover:scale-105 border-b-2 border-cyan-500 transition-all duration-100 pl-1"
+      >
         <FaMoneyBillAlt></FaMoneyBillAlt>Medical Bill
       </button>
     </div>
