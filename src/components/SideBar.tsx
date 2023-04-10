5f57a70dc3db39904ada3f5197f5d9559f574b64
Afzal Nomani <afzalnomani1@gmail.com>
2023-04-10 12:30:50 +0530

    svg added to component import not work

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -64,7 +64,37 @@ const SideBar = () => {
         onClick={() => setIsOpen(!isOpen)}
         className="absolute -right-16 top-4"
       >
-        {isOpen ? <CloseNavBtn /> : <OpenNavBtn />}
+        {isOpen ? (
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={1.5}
+            stroke="white"
+            className="w-12 h-12 self-center ml-4"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              d="M6 18L18 6M6 6l12 12"
+            />
+          </svg>
+        ) : (
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={1.5}
+            stroke="white"
+            className="w-12 h-12"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
+            />
+          </svg>
+        )}
       </button>
       <div className="flex justify-center">
         <input
