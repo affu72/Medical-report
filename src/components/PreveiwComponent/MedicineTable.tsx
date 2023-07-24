b22c941489e3efced432ca06941ea5ab3a09bf19
Afzal Nomani <afzalnomani1@gmail.com>
2023-07-24 23:13:32 +0530

    fix visiblity

--- src/components/PreveiwComponent/MedicineTable.tsx
+++ src/components/PreveiwComponent/MedicineTable.tsx
@@ -7,25 +7,33 @@ function MedicineTable() {
 
   const medicines: IMedicine[] = watch("medicines");
   return (
-    <table className="w-2/3">
-      <thead className="  bg-blue-300">
-        <tr>
-          <th className="border border-blue-500 px-4 py-2">S.No.</th>
-          <th className="border border-blue-500 px-4 py-2">Medicine Name</th>
-          <th className="border border-blue-500 px-4 py-2">Dosage</th>
-          {/* <th className="border border-blue-500 px-4 py-2">Quantity</th> */}
-        </tr>
-      </thead>
-      <tbody>
-        {medicines.map((medicine, i) => (
-          <tr key={medicine.name}>
-            <td className="border px-4 py-2 w-">{i + 1}</td>
-            <td className="border px-4 py-2">{`${medicine.type}  ${medicine.name}`}</td>
-            <td className="border px-4 py-2">{`${medicine.dose}`}</td>
-          </tr>
-        ))}
-      </tbody>
-    </table>
+    <>
+      {medicines.length >= 1 &&
+        medicines[0].name !== "" &&
+        medicines[0].dose !== "" && (
+          <table className="w-2/3">
+            <thead className="  bg-blue-300">
+              <tr>
+                <th className="border border-blue-500 px-4 py-2">S.No.</th>
+                <th className="border border-blue-500 px-4 py-2">
+                  Medicine Name
+                </th>
+                <th className="border border-blue-500 px-4 py-2">Dosage</th>
+                {/* <th className="border border-blue-500 px-4 py-2">Quantity</th> */}
+              </tr>
+            </thead>
+            <tbody>
+              {medicines.map((medicine, i) => (
+                <tr key={medicine.name}>
+                  <td className="border px-4 py-2 w-">{i + 1}</td>
+                  <td className="border px-4 py-2">{`${medicine.type}  ${medicine.name}`}</td>
+                  <td className="border px-4 py-2">{`${medicine.dose}`}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        )}
+    </>
   );
 }
 
